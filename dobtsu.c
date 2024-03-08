c8ddba5ddd01c25aabd7ffafbed7cd72528b1d87
Ana Clara <anaclararrc@hotmail.com>
2024-03-07 21:52:01 -0300

    (Version 1.0) Just some tests.

--- dobtsu.c
+++ dobtsu.c
@@ -4,34 +4,39 @@ int main() {
   int screenWidth = 1920;
   int screenHeight = 1080;
   InitWindow(screenWidth, screenHeight, "Raylib basic window");
+  Texture2D arvore = LoadTexture("arvore.png");    
   Texture2D shiba = LoadTexture("dog.png");
-  Vector2 position = { 166.0f, 265.0f };
-  Rectangle frameRec = { 0.0f, 0.0f, (float)shiba.width/5, (float)shiba.height };
+  Vector2 position = { 320.0f, 300.0f };
+  Rectangle frameRec = { 0.0f, 0.0f, (float)shiba.width/8.01, (float)shiba.height };
   int currentFrame = 0;
 
   int framesCounter = 0;
-  int framesSpeed = 8;  
-  SetTargetFPS(30);
+  int framesSpeed = 10;  
+  SetTargetFPS(40);
   
   while (!WindowShouldClose()) {
     BeginDrawing();
+    
     ClearBackground(RAYWHITE);
     framesCounter++;
 
-    if (framesCounter >= (30/framesSpeed))
+    DrawTexture(arvore, 0,0, WHITE);
+    DrawText("DOBOTSU ", 330, 200, 20, WHITE);
+    
+    if (framesCounter >= (40/framesSpeed))
     {
             framesCounter = 0;
             currentFrame++;
 
-            if (currentFrame > 5) currentFrame = 0;
+            if (currentFrame > 8) currentFrame = 0;
 
-            frameRec.x = (float)currentFrame*(float)shiba.width/5;
+            frameRec.x = (float)currentFrame*(float)shiba.width/8.01;
     }
     
     frameRec.x++;
 
     DrawTextureRec(shiba, frameRec, position, WHITE);
-    DrawText("Teste ", 20, 20, 20, BLACK);
+    
     EndDrawing();
   }
   CloseWindow();

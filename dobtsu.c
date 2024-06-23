fe08a8f6459b082efe3ac3cf4fc391ad4dc12b38
Ana Clara R. Rodrigues da Cunha <anaclararrc@hotmail.com>
2024-06-22 22:41:57 -0300

    (Version 2.0) Adding new files + presentation files

--- dobtsu.c
+++ dobtsu.c
@@ -1,82 +1,220 @@
 #include "raylib.h"
 
-int main() {
-  int currentScreen = 0;
-  int screenWidth = 480;
-  int screenHeight = 720;
-  InitWindow(screenWidth, screenHeight, "Raylib basic window");
-  Texture2D fundo_inicial = LoadTexture("fundo_inicial.png");    
-  Texture2D gelo = LoadTexture("gelo.png");  
-  Rectangle button = {150, 300, 200, 50};
-  Color buttonColor = RED;
-  int colorIndex = 0;
-  Vector2 position = { 0.0f, 0.0f };
-  Rectangle frameRec = { 0.0f, 0.0f, (float)screenWidth, (float)screenHeight};
-  int currentFrame = 0;
-  
+#define MAX_GIF_FRAMES 9
 
-  int framesCounter = 0;
-  int framesSpeed = 10;  
-  SetTargetFPS(20);
-  
-  while (!WindowShouldClose()) {
-    BeginDrawing();
+typedef enum { SCREEN_TITLE, SCREEN_GAMEPLAY, SCREEN_ENDING } GameScreen;
+
+
+int main(void)
+{
     
-    ClearBackground(RAYWHITE);
-    framesCounter++;
-    
-    switch(currentScreen){
-        case 0:
-    //Screen do shiba, animação **corrigir frames 
-        if (framesCounter >= (50/framesSpeed))
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    //FRAMES DO FUNDO
+    Texture2D gifFrames[MAX_GIF_FRAMES];
+    for (int i = 0; i < MAX_GIF_FRAMES; i++) {
+        char fileName[50];
+        sprintf(fileName, "resources/%d.png", i + 1);
+        gifFrames[i] = LoadTexture(fileName);
+    }
+    int currentGifFrame = 0;
+    int gifFrameCounter = 0;
+    int gifFrameSpeed = 20;
+
+    //BOLA
+    Vector2 ballPosition = { screenWidth/2, screenHeight/2 };
+    float ballRadius = 20.0f;
+    int ballSpeedX = 6;
+    int ballSpeedY = -4;
+
+    //AZUL
+    Rectangle player = { 10, screenHeight/2 - 50, 25, 100 };
+    float playerSpeed = 8.0f;
+    int playerScore = 0;
+
+    //VERDE
+    Rectangle enemy = { screenWidth - 10 - 25, screenHeight/2 - 50, 25, 100 };
+    float enemySpeed = 8.0f;
+    int enemyVisionRange = screenWidth/2;
+    int enemyScore = 0;
+
+    Font fntTitle = LoadFontEx("resources/pixantiqua.ttf", 12, 0, 0);
+    SetTextureFilter(fntTitle.texture, TEXTURE_FILTER_POINT);
+
+    Sound fxStart = LoadSound("resources/start.wav");
+    Sound fxPong = LoadSound("resources/pong.wav");
+
+    Music ambient = LoadMusicStream("resources/qt-plimp.xm");
+    PlayMusicStream(ambient);
+
+    bool pause = false;
+    bool finishGame = false;
+    int framesCounter = 0;
+    GameScreen currentScreen = SCREEN_TITLE; // 0-INICIO, 1-GAMEPLAY, 2-FIM
+
+    SetTargetFPS(60); 
+
+   //LOOP PRINCIPAL
+    while (!WindowShouldClose() && !finishGame) // Detectar botão de fechar janela ou tecla ESC
     {
-            framesCounter = 0;
-            currentFrame++;
+      
 
-            if (currentFrame > 100) currentFrame = 0;
+        switch (currentScreen)
+        {
+            case SCREEN_TITLE:
+            {
+                framesCounter++;
 
-            frameRec.x = (float)currentFrame * (float)(screenWidth);
-    }
-        DrawTextureRec(fundo_inicial, frameRec, position, WHITE);
-        break;
-        default:
-    //Screen do penguin
-           DrawTexture(gelo, 0,0, WHITE);
-        break;
-    }
-    
-    if(IsKeyPressed (KEY_ENTER)){
-        if(currentScreen < 1){
-            currentScreen++;
-        }else{
-            currentScreen = 0;
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = SCREEN_GAMEPLAY;
+                }
+            } break;
+            case SCREEN_GAMEPLAY:
+            {
+                if (!pause)
+                {
+                    ballPosition.x += ballSpeedX;
+                    ballPosition.y += ballSpeedY;
+
+                    if (((ballPosition.x + ballRadius) > screenWidth) || ((ballPosition.x - ballRadius) < 0))
+                    {
+                        PlaySound(fxPong);
+                        ballSpeedX *= -1;
+                    }
+
+                    if (((ballPosition.y + ballRadius) > screenHeight) || ((ballPosition.y - ballRadius) < 0))
+                    {
+                        PlaySound(fxPong);
+                        ballSpeedY *= -1;
+                    }
+
+                    if ((ballPosition.x - ballRadius) <= 0) enemyScore += 1000;
+                    else if ((ballPosition.x + ballRadius) > GetScreenWidth()) playerScore += 1000;
+
+                    if (IsKeyDown(KEY_UP)) player.y -= playerSpeed;
+                    else if (IsKeyDown(KEY_DOWN)) player.y += playerSpeed;
+
+                    if (player.y <= 0) player.y = 0;
+                    else if ((player.y + player.height) >= screenHeight) player.y = screenHeight - player.height;
+
+                    if (CheckCollisionCircleRec(ballPosition, ballRadius, player))
+                    {
+                        PlaySound(fxPong);
+                        ballSpeedX *= -1;
+                    }
+
+                    if (IsKeyDown(KEY_W)) enemy.y -= enemySpeed;
+                    else if (IsKeyDown(KEY_S)) enemy.y += enemySpeed;
+
+                    if (enemy.y <= 0) enemy.y = 0;
+                    else if ((enemy.y + enemy.height) >= screenHeight) enemy.y = screenHeight - enemy.height;
+
+                    if (CheckCollisionCircleRec(ballPosition, ballRadius, enemy))
+                    {
+                        PlaySound(fxPong);
+                        ballSpeedX *= -1;
+                    }
+                }
+                    
+                //PAUSA O JOGO COM P
+                if (IsKeyPressed(KEY_P)) pause = !pause;
+
+                if (IsKeyPressed(KEY_ENTER)) currentScreen = SCREEN_ENDING;
+            } break;
+            case SCREEN_ENDING:
+            {
+                //FIM DO JOGO COM ENTER
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    finishGame = true;
+                }
+            } break;
+            default: break;
         }
-    }
-    
-    Vector2 mousePoint = GetMousePosition();
-    bool IsMouseOverButton = CheckCollisionPointRec(mousePoint, button);
-    
-    if(IsMouseOverButton){
-        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-            if(colorIndex < 1){
-                colorIndex++;
-            }else{
-                colorIndex = 0;
-            }
-            if(colorIndex == 0){
-              buttonColor=RED;   
-            }else{
-              buttonColor=GREEN;
-            }
+
+        gifFrameCounter++;
+        
+        if (gifFrameCounter >= gifFrameSpeed) {
+            gifFrameCounter = 0;
+            currentGifFrame++;
+            if (currentGifFrame >= MAX_GIF_FRAMES) currentGifFrame = 0;
         }
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            //FUNDO
+            DrawTexture(gifFrames[currentGifFrame], 0, 0, WHITE);
+
+            switch (currentScreen)
+            {
+                case SCREEN_TITLE:
+                {
+                    DrawTextEx(fntTitle, "DOBUTSU", (Vector2){ 200, 100 }, fntTitle.baseSize*6, 4, WHITE);
+
+                    if ((framesCounter/30)%2) DrawText("PRESSIONE ENTER para COMEÇAR", 200, 300, 30, WHITE);
+
+                } break;
+                
+                case SCREEN_GAMEPLAY:
+                {
+                    DrawCircleV(ballPosition, ballRadius, RED);
+
+                    DrawRectangleRec(player, BLUE);
+
+                    DrawRectangleRec(enemy, DARKGREEN);
+
+                    DrawLine(enemyVisionRange, 0, enemyVisionRange, screenHeight, GRAY);
+
+                    DrawText(TextFormat("%04i", playerScore), 100, 10, 30, BLUE);
+                    DrawText(TextFormat("%04i", enemyScore), screenWidth - 200, 10, 30, DARKGREEN);
+
+                    if (pause)
+                    {
+                        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, 0.8f));
+                        DrawText("JOGO PAUSADO", 320, 200, 30, WHITE);
+                    }
+                } break;
+                
+                case SCREEN_ENDING:
+                {
+                    
+                    
+                    if (enemyScore > playerScore){
+                      DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                      DrawText("FIM DE JOGO", 10, 10, 30,WHITE);
+                      DrawText(TextFormat("Jogador VERDE ganha com %d pontos!", enemyScore), screenWidth / 2 - MeasureText(TextFormat("Jogador VERDE ganha com %d pontos!", enemyScore), 20) / 2, screenHeight / 2, 20, WHITE);
+                    } 
+                    else if (playerScore > enemyScore){
+                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                     DrawText("FIM DE JOGO", 10, 10, 30,WHITE);
+                     DrawText(TextFormat("Jogador AZUL ganha com %d pontos!", playerScore), screenWidth / 2 - MeasureText(TextFormat("Jogador AZUL ganha com %d pontos!", playerScore), 20) / 2, screenHeight / 2, 20, WHITE);
+                   }
+                   else{
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("FIM DE JOGO", 10, 10, 30,WHITE);
+                    const char* empateMsg = "Houve um EMPATE! Jogue novamente!";
+                      DrawText(empateMsg, screenWidth / 2 - MeasureText(empateMsg, 20) / 2, screenHeight / 2, 20, WHITE);
+                   }
+                    
+                } break;
+                default: break;
+            }
+
+        EndDrawing();
     }
-    
-    DrawRectangleRec(button, buttonColor);
-    DrawText("NOVO JOGO", button.x + 10, button.y + 10, 20, BLACK);
-   
-    
-    EndDrawing();
-  }
-  CloseWindow();
-  return 0;
+
+    for (int i = 0; i < MAX_GIF_FRAMES; i++) {
+        UnloadTexture(gifFrames[i]);
+    }
+    UnloadFont(fntTitle);
+
+    CloseWindow(); //FECHA O JOGO
+
+    return 0;
 }
